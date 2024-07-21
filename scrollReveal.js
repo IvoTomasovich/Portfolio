0f4e630f19780a7d0573d306de9df235e5587783
Ivo Tomasovich <ivotomasovich@gmail.com>
2024-07-21 18:53:37 -0500

    commit

--- scrollReveal.js
+++ scrollReveal.js
@@ -1,8 +1,8 @@
-ScrollReveal().reveal('.section-content', {
-    origin: 'bottom',   // Starting position
-    distance: '20px',   // Distance moved
-    duration: 1000,     // Animation duration
-    delay: 300,         // Delay before reveal
-    easing: 'ease-out', // Easing function
-    interval: 200       // Interval between elements (if multiple)
-});
+// ScrollReveal().reveal('.section-content', {
+//     origin: 'bottom',   // Starting position
+//     distance: '20px',   // Distance moved
+//     duration: 1000,     // Animation duration
+//     delay: 300,         // Delay before reveal
+//     easing: 'ease-out', // Easing function
+//     interval: 200       // Interval between elements (if multiple)
+// });
